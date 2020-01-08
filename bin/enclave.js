9095a9622bb12b791c95901b4eed281d4bd77ae0
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2020-01-08 20:09:40 +0100

    Add cryptographic identity

--- bin/enclave.js
+++ bin/enclave.js
@@ -1,4 +1,10 @@
+var { number } = require('stdopt')
 var run = require('stdrun')
 var serve = require('../lib/servers/app')
 
-run(serve)
+function enclave (opts = {}) {
+  var port = number(opts.port).or(opts.p).value()
+  serve({ port })
+}
+
+run(enclave)
