2b65318ddd9b802f6354d996d5b32ce2ef3cb6d0
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2020-01-15 08:30:09 +0100

    Pass in custom name from command line

--- bin/enclave.js
+++ bin/enclave.js
@@ -1,10 +1,12 @@
-var { number } = require('stdopt')
+var { number, string } = require('stdopt')
 var host = require('../lib/host')
 var run = require('stdrun')
 
 function enclave (opts = {}) {
+  var name = string(opts.name).or(opts.n).or('Incognito').value()
   var port = number(opts.port).or(opts.p).value()
-  host({ port })
+
+  host({ name, port })
 }
 
 run(enclave)
