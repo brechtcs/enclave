c5abf4eada2c135d06fcb18d629cf2ce634d6ee3
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2020-02-08 22:23:15 +0100

    Persist host configuration

--- bin/enclave.js
+++ bin/enclave.js
@@ -1,12 +1,19 @@
-var { number, string } = require('stdopt')
+var { boolean, number, string } = require('stdopt')
+var db = require('../lib/data')
 var host = require('../lib/host')
 var run = require('stdrun')
 
-function enclave (opts = {}) {
-  var name = string(opts.name).or(opts.n).or('Incognito').value()
+async function enclave (opts = {}, name) {
+  var create = boolean(opts.create).or(false).value()
   var port = number(opts.port).or(opts.p).value()
+  name = string(name).value()
 
-  host({ name, port })
+  if (create) {
+    await db.create(name)
+  }
+
+  await db.open(name, port)
+  host({ port })
 }
 
 run(enclave)
