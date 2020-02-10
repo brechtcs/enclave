cca6d60eb7a050d649a9e3059661f9ce14f03ac7
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2020-02-10 22:20:05 +0100

    Improve choosing of port

--- bin/enclave.js
+++ bin/enclave.js
@@ -1,19 +1,25 @@
 var { boolean, number, string } = require('stdopt')
 var db = require('../lib/data')
 var host = require('../lib/host')
+var local = require('../lib/local')
 var run = require('stdrun')
 
 async function enclave (opts = {}, name) {
   var create = boolean(opts.create).or(false).value()
-  var port = number(opts.port).or(opts.p).value()
+  var port = number(opts.port).or(opts.p).raw()
   name = string(name).value()
 
   if (create) {
-    await db.create(name)
+    await db.create(name, port)
   }
-
   await db.open(name, port)
-  host({ port })
+
+  host({ port }).on('listening', function () {
+    var a = this.address()
+    var msg = `Enclave is running on port ${a.port}`
+    local.announce(a.port)
+    console.log(msg)
+  })
 }
 
 run(enclave)
