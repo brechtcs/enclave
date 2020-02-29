93f6c134fb4213bce92b630793e61b5adc4059a8
Brecht Savelkoul <brecht.savelkoul@gmail.com>
2020-02-29 13:55:49 +0100

    Move request filtering to `local` module

--- app/local.js
+++ app/local.js
@@ -1,7 +1,9 @@
 var Host = require('../models/host')
 var bonjour = require('bonjour')()
+var client = require('client-ip')
 var exit = require('async-exit-hook')
 var guests = require('./guests')
+var ip = require('ip')
 var type = 'enclave'
 
 module.exports.announce = function (port) {
@@ -14,4 +16,16 @@ module.exports.announce = function (port) {
   local.on('up', guests.gateway.join)
   local.on('down', guests.leave)
   exit(service.stop)
+}
+
+module.exports.filter = function (req, res, next) {
+  if (req.method === 'GET' || isLocal(req)) {
+    return next()
+  }
+  res.writeHead(403)
+  res.end('forbidden')
+}
+
+function isLocal (req) {
+  return ip.isLoopback(client(req))
 }
